ad4b750a519b65c057344d304b93f667f2434173
vanphamh2 <komusic.vn@gmail.com>
2017-07-01 10:36:18 +0700

    Update fbcomment.js

--- fbcomment.js
+++ fbcomment.js
@@ -2,6 +2,6 @@
 var js, fjs = d.getElementsByTagName(s)[0];
 if (d.getElementById(id)) return;
 js = d.createElement(s); js.id = id;
-js.src = "//connect.facebook.net/vi_VN/all.js#xfbml=1&appId=239607966529665";
+js.src = "//connect.facebook.net/vi_VN/all.js#xfbml=1&appId=1384713378283042";
 fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
