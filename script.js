a337961c7acdbb9f9b77895c9e216e990e2920b5
vanphamh2 <komusic.vn@gmail.com>
2017-08-11 18:53:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1280,7 +1280,7 @@ $document.ready(function () {
             }
           },
           {
-            breakpoint: 480,
+            breakpoint: 413,
             settings: {
               centerMode: $slickItem.attr("data-sm-center-mode") == "true",
               centerPadding: $slickItem.attr("data-sm-center-padding") ? $slickItem.attr("data-sm-center-padding") : '0',
